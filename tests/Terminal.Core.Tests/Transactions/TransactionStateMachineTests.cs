a30efdabf66bcddfc40bb98c14d5ab45a434f8f4
velma-V1 <chynnabell2015@gmail.com>
2026-09-01 16:08:23 -0400

    test(v3): define transaction lifecycle

---
+++ tests/Terminal.Core.Tests/Transactions/TransactionStateMachineTests.cs
@@ -0,0 +1,52 @@
+using Terminal.Core.Transactions;
+
+namespace Terminal.Core.Tests.Transactions;
+
+public sealed class TransactionStateMachineTests
+{
+    [Theory]
+    [InlineData(TransactionState.Prepared, TransactionState.Authorized)]
+    [InlineData(TransactionState.Authorized, TransactionState.Started)]
+    [InlineData(TransactionState.Started, TransactionState.SideEffectObserved)]
+    [InlineData(TransactionState.SideEffectObserved, TransactionState.Verifying)]
+    [InlineData(TransactionState.Started, TransactionState.Verifying)]
+    [InlineData(TransactionState.Verifying, TransactionState.Committed)]
+    [InlineData(TransactionState.Started, TransactionState.Failed)]
+    [InlineData(TransactionState.Started, TransactionState.Cancelled)]
+    [InlineData(TransactionState.Started, TransactionState.Indeterminate)]
+    [InlineData(TransactionState.Failed, TransactionState.RollingBack)]
+    [InlineData(TransactionState.Cancelled, TransactionState.RollingBack)]
+    [InlineData(TransactionState.Indeterminate, TransactionState.RollingBack)]
+    [InlineData(TransactionState.RollingBack, TransactionState.RolledBack)]
+    [InlineData(TransactionState.Failed, TransactionState.Compensating)]
+    [InlineData(TransactionState.Compensating, TransactionState.Compensated)]
+    public void Legal_transition_is_allowed(TransactionState from, TransactionState to)
+    {
+        Assert.True(TransactionStateMachine.CanTransition(from, to));
+        Assert.Equal(to, TransactionStateMachine.Transition(from, to));
+    }
+
+    [Theory]
+    [InlineData(TransactionState.Prepared, TransactionState.Committed)]
+    [InlineData(TransactionState.Committed, TransactionState.Started)]
+    [InlineData(TransactionState.RolledBack, TransactionState.Committed)]
+    [InlineData(TransactionState.Compensated, TransactionState.Started)]
+    [InlineData(TransactionState.Prepared, TransactionState.RolledBack)]
+    public void Impossible_transition_is_rejected(TransactionState from, TransactionState to)
+    {
+        Assert.False(TransactionStateMachine.CanTransition(from, to));
+        Assert.Throws<InvalidOperationException>(() => TransactionStateMachine.Transition(from, to));
+    }
+
+    [Theory]
+    [InlineData(TransactionState.Committed)]
+    [InlineData(TransactionState.RolledBack)]
+    [InlineData(TransactionState.Compensated)]
+    public void Terminal_state_has_no_outgoing_transition(TransactionState state)
+    {
+        foreach (var candidate in Enum.GetValues<TransactionState>())
+        {
+            Assert.False(TransactionStateMachine.CanTransition(state, candidate));
+        }
+    }
+}
